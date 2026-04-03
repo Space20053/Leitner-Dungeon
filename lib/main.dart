49907e43187bc6a4345d4c72b98266b2c85ed1d8
Doctor Space <userer06@gmail.com>
2026-04-03 18:31:31 +0300

    ci: add GitHub Actions workflow for Flutter Web deploy

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ import 'presentation/screens/map_screen.dart';
 import 'presentation/screens/battle_screen.dart';
 import 'presentation/screens/add_word_screen.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'dart:io';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 final router = GoRouter(
   initialLocation: '/',
@@ -29,7 +30,7 @@ final router = GoRouter(
 );
 
 void main() {
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
